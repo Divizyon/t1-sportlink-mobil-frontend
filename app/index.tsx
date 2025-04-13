79150e93654e6e338ea49373e513aec1d9978c7a
eren iridere <eiridere@gmail.com>
2025-04-13 18:25:38 +0300

    feat:Integrated gluestack UI structure into the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Box className="bg-primary-500 p-5 rounded-lg">
+        <Text className="text-typography-0">This is the Box</Text>
+      </Box>
     </View>
   );
 }
