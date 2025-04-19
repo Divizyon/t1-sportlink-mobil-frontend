797c317ec233af404ad05eb09b06f0199ecd0694
eren iridere <eiridere@gmail.com>
2025-04-19 13:05:24 +0300

    Completed signup and signin screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,12 @@ import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
-  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </GluestackUIProvider>
+  );
 }
